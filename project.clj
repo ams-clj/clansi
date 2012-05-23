df21a1e422e5b275f5d763201d6c8b746102cb7d
Paul Legato <pjlegato@builtonlogic.com>
2012-05-22 18:28:20 -0700

    Bump version to 1.3.0 stable

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clansi "1.3.0-SNAPSHOT"
+(defproject org.clojars.pjlegato/clansi "1.3.0"
   :description "ANSI style and color codes for Clojure."
   :plugins [[lein-swank "1.4.4"]]
   :dependencies [[org.clojure/clojure "1.3.0"]])
